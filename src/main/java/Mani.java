b739ebb8264a3aaa4544357f584d16eac428a80f
KEATLLoevkilde <thom4a14@stud.kea.dk>
2022-11-14 09:30:39 +0100

    Greated Main

---
+++ src/main/java/Mani.java
@@ -0,0 +1,9 @@
+public class Mani {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        dog.makeSound();
+        cat.makeSound();
+    }
+}
